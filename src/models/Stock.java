63db9c70b96718de52378762967af1b34ae2d7d5
SwayGatsby <kayla.fowler@hotmail.com>
2017-12-03 16:52:23 +0000

    finished stock class, all tests passing

--- src/models/Stock.java
+++ src/models/Stock.java
@@ -1,7 +1,7 @@
 package models;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Stock {
     private ArrayList<Product> stockList;
@@ -16,6 +16,32 @@ public class Stock {
 
     public int numOfProducts() {
         return stockList.size();
+    }
+
+    public void addProduct(Product product) {
+        stockList.add(product);
+    }
+
+    public void removeProduct(Product product) {
+        stockList.remove(product);
+    }
+
+    public double totalPriceOfProducts() {
+        double total = 0;
+
+        for(Product product: stockList) {
+            total += product.getPrice();
+        }
+        return total;
+    }
+
+    public int numOfProductInStock(Product product){
+        int occurrences = Collections.frequency(stockList,product);
+        return occurrences;
+    }
+
+    public void emptyStockList(){
+        stockList.clear();
     }
 }
 
